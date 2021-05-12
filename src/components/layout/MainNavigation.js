a9f770464f4ca8f50fa143917121d733a2bf134f
Irene L <irene_lee@live.com.sg>
2021-05-12 19:27:31 +0800

    Added useContext

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
 import {Link} from 'react-router-dom';
 import classes from './MainNavigation.module.css';
+import FavouritesContext from '../../store/favourites-context';
 
 function MainNavigation() {
+    const favouritesCtx = useContext(FavouritesContext);
+
     return (
     <header className={classes.header}>
         <div className={classes.logo}>
@@ -13,10 +17,13 @@ function MainNavigation() {
                     <Link to='/'>All Meetups</Link>
                 </li>
                 <li>
-                    <Link to='/new-meetup'>New Meetup</Link>
+                    <Link to='/new-meetup'>Add New Meetup</Link>
                 </li>
                 <li>
-                    <Link to='/favourites'>Favourites</Link>
+                    <Link to='/favourites'>
+                        Favourites
+                        <span className={classes.badge}>{favouritesCtx.totalFavourites}</span>
+                        </Link>
                 </li>
             </ul>
         </nav>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,30 @@
+import { useHistory } from 'react-router-dom';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 function NewMeetupPage() {
-    return <div>New Meetup Page</div>
+    const history = useHistory();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react21-f9bbb-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+            {
+                method: 'POST', 
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            ).then(() => {
+                history.replace('/');
+            });
+    }
+
+    return (
+    <section>
+        <h1>Add New Meetup </h1>
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+    </section>
+    );
 }
 
 export default NewMeetupPage;
